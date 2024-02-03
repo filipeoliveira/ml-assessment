b1269f4466f16b5217ec4c1be93bb8823361e7ef
Filipe Oliveira <filipe.jtoliveira@gmail.com>
2024-02-03 02:28:51 -0300

    fix: removing unused folders and files

--- src/Cache/CacheInterface.php
+++
@@ -1,9 +0,0 @@
-<?php 
-namespace App\Cache;
-
-interface CacheInterface {
-    public function get($key);
-    public function set($key, $value, $ttl = null);
-    public function delete($key);
-    // TODO -  add other necessary methods
-}
--- src/Utilities/ResponseHelper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Response;
-use App\Utilities\Validator;
-
-function response()
-{
-    return new Response();
-}
