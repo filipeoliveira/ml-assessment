b006cfeda404e36276d629800df4d78820a5a7c4
Filipe Oliveira <filipe.jtoliveira@gmail.com>
2024-02-04 18:01:19 -0300

    changes on index.php and bootstrap file

--- app/index.php
+++ app/index.php
@@ -5,16 +5,14 @@
  * It sets up error handling, loads the routes, and dispatches the request.
  */
 
-require_once 'src/Utilities/Errors/ErrorHandler.php';
+require_once __DIR__ . '/src/setup.php';
 
-use App\Utilities\Errors\ErrorHandler;
-
-new ErrorHandler();
+use App\BootstrapContainer;
 
 // Load the routes
-$container = require_once __DIR__ . '/src/bootstrap.php';
+$bootstrap = new BootstrapContainer();
+$container = $bootstrap->getContainer();
 $router = require_once "routes.php";
-
 
 $router->setContainer($container);
 $router->handleRequest();
